01c32620c59a52423db8591a15b42fd2b6da5e41
Lisias T <me@lisias.net>
2019-01-26 23:21:47 -0200

    Versioning Normalizing

--- Source/B9-PWings/Properties/AssemblyInfo.cs
+++ Source/B9-PWings/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("B9 PWings Fork")]
+[assembly: AssemblyTitle("B9 PWings")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("B9 PWings Fork")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("B9 PWings /L Unofficial")]
+[assembly: AssemblyCopyright("Copyright © 2019 LisiasT")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.50.0.0")]
-[assembly: AssemblyFileVersion("0.50.0.0")]
+[assembly: AssemblyVersion(WingProcedural.Version.Number)]
+[assembly: AssemblyFileVersion(WingProcedural.Version.Number)]
+
